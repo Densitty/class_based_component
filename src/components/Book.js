84003ff97d02997b677755dd43a22649aad0e8ef
Densitty <dennis.brough@yandex.com>
2021-04-07 06:01:47 -0500

    rendering conditional statements through either a function or a ternary operator or using the AND operator

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,15 +3,30 @@ import React, { Component } from "react";
 export default class Book extends Component {
   state = {
     count: 0,
+    showInfo: false,
+  };
+
+  handleInfo = () => {
+    this.setState({
+      showInfo: !this.state.showInfo,
+    });
   };
 
   render() {
     console.log(this.props);
     // by the power of destructuring
-    const { id, title, author, image } = this.props.list;
+    const { title, author, image } = this.props.list;
 
     // const { handleDelete } = this.props;
     /* list is the name attribute on Book in BookList */
+
+    const checkInfo = (info, book_title, book_name) => {
+      if (info) {
+        return `Readers are learners and learners are reader, goes a wise saying. We value knowledge, which is why we have brought you this book, titled, ${book_title}, authored by ${book_name} for your reading pleasure.`;
+      } else {
+        return null;
+      }
+    };
 
     return (
       <article className="book">
@@ -19,11 +34,30 @@ export default class Book extends Component {
         <div>
           <h3>Title: {title}</h3>
           <h4>Author: {author}</h4>
-          <button type="button" onClick={() => this.props.delete(id)}>
-            delete me
+          <button type="button" onClick={this.handleInfo}>
+            toggle info
           </button>
+          {/* using function to render a conditional statement */}
+          {<p>{checkInfo(this.state.showInfo, title, author)}</p>}
+          {/* using ternary operator */}
+          {/* {this.state.showInfo
+            ? "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatum, aliquid amet ad magni consequatur?"
+            : null} */}
+          {/* using AND operator to display info */}
+          {/* {this.state.showInfo && (
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+              voluptatum, aliquid amet ad magni consequatur
+            </p>
+          )} */}
         </div>
       </article>
     );
   }
 }
+
+/* 
+{this.state.showInfo
+              ? "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatum, aliquid amet ad magni consequatur?"
+              : null}
+*/
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -12,42 +12,12 @@ export default class BookList extends Component {
     };
   }
 
-  /* handleDelete = (id) => {
-    // filter through and return the book item(s) which matches not the id passed
-    const sortedBooks = this.state.books.filter((book) => {
-      console.log(book);
-      return book.id !== id;
-    });
-
-    // 
-    this.setState({
-      books: sortedBooks,
-    });
-  };
- */
-  handleDelete = (id) => {
-    const bookToRemoveID = this.state.books.findIndex((book) => {
-      return book.id === id;
-    });
-
-    this.state.books.splice(bookToRemoveID, 1);
-
-    /* console.log(bookToRemoveID);
-    const newBooks = [...this.state.books];
-    newBooks.splice(bookToRemoveID, 1); */
-
-    this.setState({
-      books: this.state.books,
-      /* books: newBooks */
-    });
-  };
-
   render() {
     return (
       <section>
         <h3>This is our Booklist</h3>
         {this.state.books.map((item) => {
-          return <Book list={item} key={item.id} delete={this.handleDelete} />;
+          return <Book list={item} key={item.id} />;
         })}
       </section>
     );
