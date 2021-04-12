b98bc9ed879e6d6c1c593aa0accdc22c7cffc238
Densitty <dennis.brough@yandex.com>
2021-04-12 04:50:11 -0500

    targeting uncontrolled inputs with refs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 import Form from "./components/Input";
 
-// class Form extends Component {
-//   render() {
-//     return <h1>Hello Form</h1>;
-//   }
-// }
 class App extends Component {
   render() {
     return <Form />;
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,88 +1,32 @@
 import React from "react";
 
 class Form extends React.Component {
-  state = {
-    firstName: "",
-    // firstName: null,
-    lastName: "",
-    // lastName: null,
-    people: [],
-  };
-
-  handleChange = (e) => {
-    // we can target an input either by value or the name attribute
-    /* console.log(e.target.value);
-    console.log(e.target.name); */
-
-    /* if (e.target.name === "firstName") {
-      const textValue = e.target.value;
-      this.setState({
-        firstName: textValue,
-      });
-    }
-
-    if (e.target.name === "lastName") {
-      const textValue = e.target.value;
-      this.setState({
-        lastName: textValue,
-      });
-    } */
-
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
   handleSubmit = (e) => {
     e.preventDefault();
 
-    const firstName = this.state.firstName;
-    const lastName = this.state.lastName;
+    const name = this.refs.myName;
+    const email = this.email;
+    const text = this.refs.myText;
 
-    // console.log(firstName, lastName);
-
-    if (firstName.length > 0 && lastName.length > 0) {
-      const person = `${firstName} ${lastName}`;
-      const personList = this.state.people;
-      personList.push(person);
-      this.setState({
-        // spread the value of people of people of people
-        // people: [...this.state.people, person],
-        people: personList,
-        firstName: "",
-        lastName: "",
-      });
-      console.log(this.state.people);
-    }
+    text.style.color = "red";
+    console.log(
+      `My name is ${name.value} and my mail address is ${email.value}`
+    );
   };
 
   render() {
     return (
       <section>
-        <article>
-          <form action="" onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              name="firstName"
-              placeholder="firstname"
-              value={this.state.firstName}
-              onChange={this.handleChange}
-            />
-            <input
-              type="text"
-              name="lastName"
-              placeholder="lastname"
-              value={this.state.lastName}
-              onChange={this.handleChange}
-            />
-            <button type="submit">submit</button>
-          </form>
-        </article>
-
-        <article>
-          <h1>People</h1>
-          <div>{this.state.people}</div>
-        </article>
+        <form action="" onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="name" ref="myName" />
+          <input
+            type="email"
+            placeholder="email"
+            ref={(param) => (this.email = param)}
+          />
+          <button type="submit">submit</button>
+        </form>
+        <p ref="myText">Hello People</p>
       </section>
     );
   }
