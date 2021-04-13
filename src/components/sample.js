eac337307bf63767395299236eb29f260490787a
Densitty <dennis.brough@yandex.com>
2021-04-13 13:36:16 -0500

    async behavior of useState

--- src/components/sample.js
+++ src/components/sample.js
@@ -3,23 +3,23 @@ import React, { useState } from "react";
 const UseState = () => {
   const [value, setValue] = useState(0);
 
-  const resetValue = () => {
-    setValue(0);
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1);
+      setValue((prevState) => {
+        console.log(prevState);
+        return prevState + 1;
+      });
+    }, 2000);
   };
 
   return (
     <>
       <section style={{ margin: "4rem 0" }}>
-        <h2>regular counter</h2>
+        <h2>complex counter</h2>
         <h1>{value}</h1>
-        <button className="btn" onClick={() => setValue(value + 1)}>
-          increase
-        </button>
-        <button className="btn" onClick={resetValue}>
-          reset
-        </button>
-        <button className="btn" onClick={() => setValue(value - 1)}>
-          decrease
+        <button className="btn" onClick={complexIncrease}>
+          complex increase
         </button>
       </section>
     </>
