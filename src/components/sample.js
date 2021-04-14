264532f04656addc5879f59516c9ea410e011b2b
Densitty <dennis.brough@yandex.com>
2021-04-14 06:02:25 -0500

    cleanup functionality in useEffect

--- src/components/sample.js
+++ src/components/sample.js
@@ -2,29 +2,13 @@ import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
-const UseEffectCleanup = () => {
-  const [width, setWidth] = useState(window.innerWidth);
-
-  const checkSize = () => {
-    setWidth(window.innerWidth);
-  };
-
-  useEffect(() => {
-    console.log("window resized to :-", width);
-    window.addEventListener("resize", checkSize);
-    /* the return fxn gets invoked immediately the eventListener runs (with useEffect, we trigger it) and get the function body rendered first before another eventListener is triggered again (thus closing down the previous resize event before the next resize is triggered) */
-    return () => {
-      console.log("cleanup");
-      window.removeEventListener("resize", checkSize);
-    };
-  });
-  console.log("component render");
+const FetchData = () => {
   return (
     <>
       <h1>UseEffect Cleanup</h1>
-      <h2>{width} px</h2>
+      <h2> px</h2>
     </>
   );
 };
 
-export default UseEffectCleanup;
+export default FetchData;
