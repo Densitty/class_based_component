01b4e7620f62b567e747cf8a0f48d4d55c280db0
Densitty <dennis.brough@yandex.com>
2021-04-14 01:54:02 -0500

    useEffect with condition and dependency list

--- src/components/sample.js
+++ src/components/sample.js
@@ -4,10 +4,14 @@ import React, { useState, useEffect } from "react";
 // second parameter
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
+  // we cannot place a hook inside a conditional; to set a conditional;
   useEffect(() => {
     console.log("call useEffect");
-    document.title = `New Messages(${value})`;
-  });
+    if (value > 1) {
+      document.title = `New Messages(${value})`;
+    }
+  }, [value]);
+  /* if the dependency is present in the array, the useEffect default behaviour will be experienced but if not (or if the array is empty), useEffect will only be rendered once */
   console.log("render component");
 
   return (
