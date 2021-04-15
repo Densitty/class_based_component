dff40fe7ce6c2613e363ffd91fa593a1c13db478
Densitty <dennis.brough@yandex.com>
2021-04-15 10:21:52 -0500

    controlled input using useRef

--- src/components/sample.js
+++ src/components/sample.js
@@ -1,86 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
 
 const ControlledInput = () => {
-  const [people, setPeople] = useState([]);
-
-  const [person, setPerson] = useState({
-    firstName: "",
-    email: "",
-    age: "",
-  });
-
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-
-    setPerson({ ...person, [name]: value });
-
-    console.log(name, value);
-  };
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    if (person.firstName && person.email && person.age) {
-      const newPerson = { ...person, id: new Date().getTime().toString() };
-      setPeople([...people, newPerson]);
-      setPerson({ firstName: "", email: "", age: "" });
-    }
+    console.log(refContainer.current.value);
   };
 
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+    // due to useRef not triggering re-render, we do not need to add a dependency list in useEffect
+  });
   return (
     <>
-      <article>
-        <form className="form">
-          <div className="form-control">
-            <label htmlFor="firstName">Name:</label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              value={person.firstName}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="form-control">
-            <label htmlFor="age">Age:</label>
-            <input
-              type="number"
-              id="age"
-              name="age"
-              value={person.age}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="form-control">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              value={person.email}
-              onChange={handleChange}
-            />
-          </div>
-
-          <button type="submit" onClick={handleSubmit}>
-            add person
-          </button>
-        </form>
-
-        {people.map((person) => {
-          const { id, firstName, email, age } = person;
-          return (
-            <div className="item" key={id}>
-              <h4>{firstName}</h4>
-              <p>{email}</p>
-              <p>{age}</p>
-            </div>
-          );
-        })}
-      </article>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" ref={refContainer} />
+          <button type="submit">submit</button>
+        </div>
+      </form>
+      <div ref={divContainer}>Hello World</div>
     </>
   );
 };
