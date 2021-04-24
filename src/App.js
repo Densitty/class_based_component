ef75e789719e49c2c96dd6b9678eeb097d1bb7f3
Densitty <dennis.brough@yandex.com>
2021-04-24 12:09:56 -0500

    starting prop drilling in hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
-import Setup from "./components/sample";
+import React, { Component } from "react";
+import BookList from "./components/BookList";
+import "./App.css";
 
-const App = () => {
-  return (
-    <div className="container">
-      <Setup />
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <BookList />
+      </section>
+    );
+  }
+}
 
 export default App;
